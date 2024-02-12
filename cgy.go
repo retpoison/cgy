7ca887ccc7fab93907bcd2d8ada35f7b2cde0742
retpoison <standardret@proton.me>
2024-02-12 20:39:47 +0330

    add log file.

--- cgy.go
+++ cgy.go
@@ -3,9 +3,18 @@ package main
 import (
 	"cgy/config"
 	"cgy/tui"
+	"log"
+	"os"
 )
 
 func main() {
 	var config *config.Config = config.GetConfig()
+	f, err := os.OpenFile(config.LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
 	tui.Run(config)
 }

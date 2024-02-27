1f7174a6a04d43f67724348fd73b4334d48865ad
retpoison <standardret@proton.me>
2024-02-27 10:26:09 +0330

    truncate log file. add None for discarding logs.

--- src/cgy.go
+++ src/cgy.go
@@ -1,6 +1,8 @@
 package cgy
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -16,14 +18,30 @@ func InitConfig() {
 }
 
 func SetLogOutput() {
+	if config.LogFile == "None" {
+		log.SetOutput(io.Discard)
+		return
+	}
+	fmt.Println(config)
 	f, err := os.OpenFile(config.LogFile, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		defer log.SetOutput(io.Discard)
+		log.Printf("error opening log file: %v", err)
+	}
+	err = os.Truncate(config.LogFile, 0)
+	if err != nil {
+		log.Printf("Failed to truncate: %v", err)
 	}
 	log.SetOutput(f)
 }
 
 func Run() {
-	defer log.Writer().(*os.File).Close()
+	defer func() {
+		switch log.Writer().(type) {
+		case *os.File:
+			log.Writer().(*os.File).Close()
+		}
+	}()
+
 	run()
 }
